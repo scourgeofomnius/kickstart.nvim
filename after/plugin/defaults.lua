0511ae07003fba5c5bd79d2754d91167bcc98fa0
scourgeofomnius <joseph.a.salmans@gmail.com>
2024-02-04 12:51:46 -0800

    added php support to lsp

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -1,6 +1,10 @@
 local builtin = require('telescope.builtin')
 vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
 vim.api.nvim_set_keymap('n', '<leader>pv', ':Ex<CR>', {noremap=true,silent=true})
 vim.keymap.set('n', '<leader>ps', function()
 		builtin.grep_string({search = vim.fn.input("Grep > ")});
 end)
+
